726afb40ff674b0aa379ecf4d1bfed7a38b5798e
kanishk610 <kanishk6106@gmail.com>
2026-02-24 17:37:52 +0000

    Basic

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,19 @@
+const username="kanishk";
+let pass=42424324;
+var sign= "Press";
+nextpage="next";
+
+console.log(username);
+console.log(pass);
+console.log(sign)
+console.log(nextpage);
+
+// username="kumar";
+// console.log(username);
+pass="hello";
+console.log(pass);
+
+sign="exit";
+console.log(sign);
+nextpage="back";
+console.log(nextpage);
