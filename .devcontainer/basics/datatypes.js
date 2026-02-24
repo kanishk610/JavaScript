a4dc71bcfe6f26c0ecabdb24d778edd10737000d
kanishk610 <kanishk6106@gmail.com>
2026-02-24 17:59:16 +0000

    DataTypes

---
+++ .devcontainer/basics/datatypes.js
@@ -0,0 +1,25 @@
+// All Types of DataTypes in JAVASCRIPT
+
+// NUMBER=13234
+// BigInt=8956389752378528528
+// Boolean=True/false
+// null=standalone value
+// String="Kanishk"
+// undefined=not assigned value
+// Symbol=Unique
+// object
+
+let age=18;
+let idnum=5353554522352325235252n;
+let isright=false;
+let rain=null;
+let name="Kanishk";
+let lastname;
+
+console.log(typeof (age));
+console.log(typeof idnum);
+console.log(typeof isright);
+console.log(typeof rain);
+console.log(typeof name);
+console.log(typeof lastname);
+
